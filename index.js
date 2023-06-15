7d33593a437e860c6eaf104e3b9265bd0bf2f365
Osaid Arif <osaida50@gmail.com>
2023-06-15 21:31:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function openform(){
+    document.getElementById("contact").style.display="block";
+    document.getElementById("contactBtn").style.display="none";
+}
+function closeform(){
+    document.getElementById("contact").style.display="none";
+    document.getElementById("contactBtn").style.display="block";
+}
